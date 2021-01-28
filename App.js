76467d6096650b5021540a152de9c088f06cedc4
Guillermo Alfaro <galfarotolon@gmail.com>
2021-01-27 19:08:58 -0500

    working on styling

--- App.js
+++ App.js
@@ -12,9 +12,9 @@ export default function App() {
 
 
   return (
-    // <WelcomeScreen />
+    <WelcomeScreen />
 
-    <ImageScreen />
+    // <ImageScreen />
 
     // <SafeAreaView style={styles.container}>
     //   <Button title='hello' />
